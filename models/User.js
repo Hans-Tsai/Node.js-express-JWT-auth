28d0b057db3a9bc7e5f3de1b1578d7b0c53d3b0a
Hans <lgs840522@gmail.com>
2021-06-21 07:48:35 +0800

    feat(ch06): add mongoose hook function on Model.save()

--- models/User.js
+++ models/User.js
@@ -17,6 +17,19 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+// Mongoose hook function: 當 `doc` 被 Model.save() 到 MongoDB 資料庫"之前"，會觸發這個函數。其中 `doc` 參數即表示"準備" save 到資料庫的那個 document
+userSchema.pre('save', function (next) {
+  // 這邊的 `this` 參數即表示"準備"要 save 到資料庫的那個本地端(local)的 User Model 實例(instance)
+  console.log('user is about to be created & saved', this);
+  next();
+});
+
+// Mongoose hook function: 當 `doc` 被 Model.save() 到 MongoDB 資料庫"之後"，會觸發這個函數。其中 `doc` 參數即表示剛剛"已經" save 到資料庫的那個 document
+userSchema.post('save', function (doc, next) {
+  console.log('new user was created & saved', doc);
+  next();
+});
+
 // mongoose.model() 方法會編譯出 (compile) 一個 user model，並自動依據第一個參數 (e.g. `user`)的複數形式
 // 到 MongoDB 資料庫中尋找名稱為 `users` 的那個 collection
 const User = mongoose.model('user', userSchema);
