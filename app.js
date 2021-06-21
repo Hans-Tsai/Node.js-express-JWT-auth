96d798d6b457072cd58fb134dd6aab396f1389a4
Hans <lgs840522@gmail.com>
2021-06-21 13:50:39 +0800

    feat(ch09): add cookie primer knowledge illustration

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
@@ -10,6 +11,8 @@ const app = express();
 app.use(express.static('public'));
 // 將 API request 夾帶的 JSON 資料"解析"成 Javascript 的物件 (object) 形式
 app.use(express.json());
+// 將 API request 夾帶的 cookie header 資料"解析"成 Javascript 的物件 (object) 形式。同時會產生 req.cookies 的屬性值，並綁定到 `request` 物件上
+app.use(cookieParser());
 
 //#endregion
 
@@ -28,5 +31,23 @@ app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 // 相當於透過 `express app` 的中介函數來套用整個設定好的 `authRoutes` 路由器物件
 app.use(authRoutes);
+
+//#endregion
+
+//#region
+/** 設定 Cookie 的鍵/值資料 */
+app.get('/set-cookies', (req, res) => {
+  // res.setHeader('Set-cookie', 'newUser=true');
+
+  res.cookie('newUser', true);
+  // 實戰中，權限驗證需在 HTTPS 中執行，因此必須加上 `secure: true` 的選項
+  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });
+  res.send('you got the cookies!');
+});
+/** 透過 cookie-parser 函式庫，將產生出來的 `req.cookies` 以 JSON 的形式回傳給 client 端 */
+app.get('/get-cookies', (req, res) => {
+  const cookies = req.cookies;
+  res.json(cookies);
+});
 
 //#endregion
